b9bf024e611ced4c1387e1f782508c847d34989e
Muhammad Adli Nursah <adlinursah@gmai.com>
2026-04-25 19:45:50 +0700

    fix akun?

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,41 +7,38 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+// 1. TAMBAHKAN 2 BARIS INI
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+
+// 2. TAMBAHKAN "implements FilamentUser" DI UJUNG BARIS INI
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    // 3. TAMBAHKAN FUNGSI INI DI PALING BAWAH (SEBELUM KURUNG TUTUP TERAKHIR)
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // Return true artinya kita mengizinkan semua akun yang terdaftar untuk masuk
+        return true; 
     }
 }
