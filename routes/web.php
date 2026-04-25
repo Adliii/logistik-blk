26082aea16eafa8fd8b43b81f23c57b3a67599ba
Muhammad Adli Nursah <adlinursah@gmai.com>
2026-04-25 17:35:05 +0700

    feat: add setup route for Kiela with migration and admin account creation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+use App\Models\User;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/setup-kiela', function () {
+        // 1. Eksekusi Migration
+        Artisan::call('migrate:fresh', ['--force' => true]);
+        
+        // 2. Bikin Akun Admin
+        User::create([
+            'name' => 'Kiela',
+            'email' => 'kiela@gmail.com',
+            'password' => bcrypt('123456789')
+        ]);
+        
+        return 'Gudang MySQL siap, Bos! Akun berhasil dibuat.';
+    });
