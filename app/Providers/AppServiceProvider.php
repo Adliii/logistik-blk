0a3512cf9e91fc9e8d20151feb76519869792d1c
Muhammad Adli Nursah <adlinursah@gmai.com>
2026-04-25 17:25:05 +0700

    fix: add URL facade and enforce HTTPS in production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // 1. TAMBAHKAN BARIS INI
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // 2. TAMBAHKAN BARIS INI AGAR CSS-NYA MUNCUL
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
